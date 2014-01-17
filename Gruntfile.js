8c7e624210c96f3bdfacba83b96a41ba4925aafa
filtercake <fabian@superpursuitmo.de>
2014-01-17 17:47:11 +0100

    compile sass with compass, live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
-module.exports = funtion(grunt){
+module.exports = function(grunt){
 	
 	grunt.initConfig({
 
-		pkg: grunt.file.readJASON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 		
 		uglify: {
 			options: {
@@ -12,15 +12,64 @@ module.exports = funtion(grunt){
 				src: 'src/<%= pkg.name %>.min.js',
 				dest: 'build/<%= pkg.name %>.min.js'
 			}
-		}
+		},
+		
+		watch: {
+      options: {
+        livereload: true,  
+      },
+      html: {
+        files: ['src/*.html'],
+        task: ['log'],
+      },
+      scripts: {
+        files: [ 'src/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: 'src/*.sass',
+        tasks: ['sass'],
+      },
+      // reload: {
+      //   files: ['**/*.html', '**/*.js', '**/*.css', '**/*.{png,jpg}'],
+      //   tasks: 'tinylr-reload'
+      // }
+      reload: {
+        files: ['src/*.html', 'src/*.js', 'src/*.css', 'src/*.{png,jpg}'],
+        tasks: 'tinylr-reload'
+      }
 
+		},
+
+    sass: {
+      dist: {
+        options: {
+                style: 'expanded',
+                lineNumbers: true,
+                compass: true,
+        },
+        files: {
+          'src/proto.css': 'src/proto.sass'
+        }
+
+      }
+    }
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
 	
-	grunt.registerTask('default', ['uglify']);
+	// grunt.registerTask('default', ['uglify']);
 
 	grunt.registerTask('log', 'Log some stuff.', function () {
 		grunt.log.write('Logging some stuff...').ok();
 	});
+
+  grunt.registerTask('reload', ['tinylr-start', 'watch']);
+	
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('tiny-lr');
 };
