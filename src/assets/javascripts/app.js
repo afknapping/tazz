c17bd8ac705ad871f31e1c7db3e766a1744fec4c
filtercake <fabian@superpursuitmo.de>
2014-02-06 21:53:20 +0100

    remove old controller stuff

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -4,38 +4,14 @@ var tazz = angular.module('tazz', ['ngAnimate']);
  
 tazz.controller('TazzCtrl', function ($scope, $http) {
 
+  $scope.planet = 'world';
+
   $http.get('/data/lib.json')
      .then(function(res){
         $scope.library = res.data;
       });
 
   // $scope.library = $scope.data;
-
-})
-
-tazz.controller('MyCtrl', function ($scope, $http) {
-
-
-
-  $scope.planet = 'world';
-
-  $scope.exampleList = [
-    'item 1',
-    'item 2',
-    'item 3',
-  ],
-  
-  $scope.exampleObjectList =[
-    {
-      "title": "Object Nr 1",
-      "uistate": false,
-    },
-    {
-      "title": "Object Nr 2",
-      "uistate": true,
-    },
-  ]; 
-
 
 })
 
@@ -46,9 +22,3 @@ tazz.controller('MyCtrl', function ($scope, $http) {
       templateUrl: '/templates/lib.html'
     };
 })
-
-
-
-
-
-
