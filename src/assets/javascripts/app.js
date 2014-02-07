b850891176a67c363f027282c29e92f8b141d84b
filtercake <fabian@superpursuitmo.de>
2014-02-07 22:36:57 +0100

    add app data and template

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -5,6 +5,16 @@ var tazz = angular.module('tazz', ['angularLocalStorage', 'ngAnimate']);
  
 tazz.controller('TazzCtrl', function ($scope, $http, storage) {
 
+  $scope.data = {};
+  $scope.getAppData = function() {
+    $http.get('/data/data.json')
+      .then(function(res){
+        $scope.data = res.data;
+        console.log('yo');
+      });
+  };
+  storage.bind($scope,'data');
+
   $scope.world = 'Tazz';
   $scope.getTazzLibrary = function() {
     $http.get('/data/tazz.json')
@@ -12,11 +22,8 @@ tazz.controller('TazzCtrl', function ($scope, $http, storage) {
         $scope.tazz = res.data;
         console.log('yo');
       });
-  }; 
-
-
+  };
   storage.bind($scope,'tazz');
-
 })
 
 .directive('lib', function() {
@@ -24,5 +31,13 @@ tazz.controller('TazzCtrl', function ($scope, $http, storage) {
       restrict: 'E',
       replace: true,
       templateUrl: '/templates/lib.html'
+    };
+})
+
+.directive('app', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/app.html'
     };
 })
