f64f1d3da7e9465813731f489241739228af71fe
filtercake <fabian@superpursuitmo.de>
2014-02-08 19:45:59 +0100

    change tazzfile to js to allow comments

---
+++ Tazzfile.js
@@ -0,0 +1,15 @@
+{
+  "templating": "jade",
+  // jade, html
+  "styling": "sass",
+  // sass, css
+  "scripting": "js",
+  // coffee, js
+  "title": "tazz-deploy-test",
+  "authors": [
+    "name": "filtercake"
+  ],
+  "deploy_path": "/tazz-deploy-test",
+
+  
+}
