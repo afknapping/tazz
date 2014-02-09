82625ee6ef486433cfcf8bf57dcd393e47e68df1
filtercake <fabian@superpursuitmo.de>
2014-02-09 02:53:12 +0100

    remotestorage exists, but not working

--- src/app.js
+++ src/app.js
@@ -1,4 +1,77 @@
 
+// http://remotestorage.io/integrate/add-to-app.html
+
+RemoteStorage.defineModule('favouriteThings', function(privateClient, publicClient) {
+
+  // Define a common data type using JSON Schema
+  privateClient.declareType('aFavouriteThing', {
+    "description": "a thing",
+    "type": "object",
+    "properties": {
+      "nameOfThing": {
+        "type": "string"
+      },
+      "isFavourite": {
+        "type": "boolean"
+      }
+    }
+  });
+
+  return {
+    exports: {
+      // Add functions for retrieving and manipulating data using
+      // methods provided by BaseClient
+      addThing: function (name) {
+        return privateClient.storeObject('aFavouriteThing', {
+          nameOfThing: name,
+          isFavourite: true
+        });
+      }
+      // define more functions...
+    }
+  };
+});
+
+
+RemoteStorage.defineModule('tasks', function(privateClient, publicClient) {
+
+  // Define a common data type using JSON Schema
+  privateClient.declareType('task', {
+    "description": "a task",
+    "type": "object",
+    "properties": {
+      "id": {
+        "type": "string",
+        "format": "id"
+      },
+      "title": {
+        "type": "string"
+      },
+      "completed": {
+        "type": "boolean"
+      }
+    }
+  });
+
+  return {
+    exports: {
+      // Add functions for retrieving and manipulating data using
+      // methods provided by BaseClient
+      addTask: function (title) {
+        var id = new Date().getTime().toString();
+        return privateClient.storeObject('task', id, {
+          id: id,
+          title: title,
+          completed: false
+        });
+      }
+      // define more functions...
+    }
+  };
+});
+
+
+
 var tazz = angular.module('tazz', ['angularLocalStorage', 'ngAnimate']);
 
 tazz.controller('AppCtrl', function ($scope, $http, storage) { 
